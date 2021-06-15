3c827bf0193bb487af440a43fa66fbb728544301
Raimundo <raimundo_l@hotmail.com>
2021-06-14 21:35:32 -0300

    iniciando

--- aula06/Untitled-2.py
+++ aula06/Untitled-2.py
@@ -1,3 +1,22 @@
-#Exemplo alterado
-print("hello world")
-print ("helo again")
+#while: 
+
+#senha = "blue123"
+#entrada = input("Digite sua senha: \n")
+#erros = 0
+#while senha != entrada:
+ #   erros += 1
+  #  entrada = input("senha incorreta \n Digite Novamente! \n")
+   # if erros ==4:
+    #    print ("cuidado! última tentativa")
+        
+   # elif erros ==5:
+    #    print ("tentativas esgotadas")
+     #   break
+      #  print("Bem Vindo!")
+#print(f"Você errou {erros} vezes")
+
+#for
+a = ("blue final")
+print(len(a))
+for i in a:
+    print(i)
