8ae71050ff397088fa1ad1095b6bdaebd33cb543
Raimundo <raimundo_l@hotmail.com>
2021-06-16 22:22:28 -0300

    exemplos aula 0

---
+++ aula08.py
@@ -0,0 +1,22 @@
+#l = [10,20,30,40,50]
+#print(l[3:])
+#print(l[1:3])
+
+
+
+# l2 = [10,20,30,40,50,60]
+# #print(l2)
+# #print(type(l2))
+# l2[3] = "pizza"
+# print(l2)
+# #for indice_atual in range(len(l2)):
+# #    l2[indice_atual] = input('digite novo valor')
+# print(l2)
+# l2.append("joint")
+# print(l2)
+
+#lista vazia:
+lvazia = []
+for i in range(5):
+    lvazia.append(input('Digite um valor: \n'))
+print(lvazia)
