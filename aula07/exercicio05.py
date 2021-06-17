12b7d6aa215b074deec14b1e516f6862d42b2ae1
Raimundo <raimundo_l@hotmail.com>
2021-06-16 22:24:43 -0300

    algumas alterações

--- aula07/exercicio05.py
+++ aula07/exercicio05.py
@@ -1,13 +1,13 @@
-from datetime import date
+# from datetime import date
 
-ano = date.today().year
-jovem = 0
-adulto_adult = 0
-for n in range(7):
-    num = int(input("Digite anos de nascimento: \n"))
-    if ano - num >= 18:
-        jovem += 1
-    elif atual - num < 21:
-        counter_young += 1
-print("Ao todo tivemos {} maiores e {} menores de idade.".format(
-    counter_adult, counter_young))
+# ano = date.today().year
+# jovem = 0
+# adulto_adult = 0
+# for n in range(7):
+#     num = int(input("Digite anos de nascimento: \n"))
+#     if ano - num >= 18:
+#         jovem += 1
+#     elif atual - num < 21:
+#         counter_young += 1
+# print("Ao todo tivemos {} maiores e {} menores de idade.".format(
+#     counter_adult, counter_young))
