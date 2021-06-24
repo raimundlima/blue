44c8a1f4bacfcf9fff0b49e3f4a432548c067112
raimundlima <77973887+raimundlima@users.noreply.github.com>
2021-06-24 12:11:12 -0300

    ideias para o projeto03

--- aula07/exercicio05.py
+++ aula07/exercicio05.py
@@ -1,13 +1,38 @@
-# from datetime import date
+from datetime import date
 
 # ano = date.today().year
 # jovem = 0
-# adulto_adult = 0
-# for n in range(7):
-#     num = int(input("Digite anos de nascimento: \n"))
-#     if ano - num >= 18:
-#         jovem += 1
-    elif atual - num < 21:
-#         counter_young += 1
-# print("Ao todo tivemos {} maiores e {} menores de idade.".format(
-#     counter_adult, counter_young))
+# adulto = 0
+# atual = 0
+# #counter_young = 0
+# #counter_adult = 0
+    
+# for n in atual:
+#         num = int(input("Digite anos de nascimento: \n"))
+# if ano - num >= 18:
+#         print('pode votar')
+# elif atual - num <= 16:
+#         print('não pode votar')
+
+
+# idade = int(input('qual é a sua idade ? \n'))
+# if idade >= 18:
+#         print('Maior de idade')
+#         elif idade <16 :
+#         print ('menor de idade')
+
+
+atual = date.today().year
+def check_age (idade):
+    
+    idade = atual - nas
+    if idade > 18:
+            print('seu voto é obrigatório')
+    elif idade >= 16 and idade <= 17:
+            print ('seu voto é opcional')
+    else:
+            print ('voce não pode votar ')
+
+nas = int(input('Por favor insira sua data de nascimento \n'))
+check_age(atual-nas)
+
---
+++ projeto03/projeto03.py
@@ -0,0 +1,53 @@
+dados = []
+numero_pessoas = 0
+continuacao = True
+feminino = 'F'
+masculino = 'M'
+dicionario = {}
+mulheres = []
+idadesLista = []
+acimaMédia = []
+
+while continuacao == True:
+    
+    nome = input('Vamos guardar alguns dados.Digite o nome de uma pessoa para cadastrá-la.\n')
+    idade = int(input('Digite a idade dessa pessoa: \n'))
+    #gen = input('Agora digite o gênero de nascença dessa pessoa. Use "F" ou "M"\n').upper()
+    
+    #while gen != masculino and gen != feminino:
+            #gen = input('Não conseguimos computar um gênero válido. Tente novamente.').upper()
+    
+    # idadesLista.append(idade)
+    # dicionario = {'Nome': nome, 'Idade': idade, 'Gênero': gen}
+
+   # if gen == feminino:
+       # mulheres.append(dicionario) 
+
+    numero_pessoas += 1
+    dados.append(dicionario)
+    confirmacao = input('Você deseja cadastrar mais alguma pessoa?\nUse "S" para Sim\nUse "N" para não\n').upper()
+    if confirmacao == 'N':
+        continuacao = False
+    elif confirmacao == 'S':
+        pass
+    else:
+        while confirmacao != 'N' and confirmacao != 'S':
+            confirmacao = input('Não conseguimos computar sua resposta. Digite novamente:\nDeseja cadastrar mais pessoas?\nUse "S" para Sim\nUse "N" para não').upper()
+    
+    
+soma = sum(idadesLista)
+mediaidade = (soma / numero_pessoas)
+
+for i in idadesLista:
+    if i > mediaidade:
+        acimaMédia.append(i)
+    
+print(dados)
+print()
+print(f'O número de pessoas cadatradas foi: {numero_pessoas}')
+print()
+print(f'A média de idade das pessoas cadastradas é: {mediaidade :.2f}')
+print()
+print(f'Esses são os registros de mulheres que foram cadastradas:\n{mulheres}')
+print()
+print(f'As idades que estão acima da média são:\n {acimaMédia}')
