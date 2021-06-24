e88c473c043e71043969e6a28bc50209b8a6fa8d
Raimundo <raimundo_l@hotmail.com>
2021-06-23 22:46:25 -0300

    exemplos de função

---
+++ aula13/funções.py
@@ -0,0 +1,24 @@
+## criando uma função ensinando ao programa a puxar a função
+
+# def somar():  #bloco de comandos da função
+#     num1 = float(input('digite um numero: '))
+#     num2 = float(input('digite outro numero: '))
+#     soma = num1 + num2
+#     print(soma)
+    
+
+def somar(numero1,numero2):  #bloco de comandos da função
+    
+    somar = numero1 * numero2
+    print(f'a area total do seu terreno é de : {somar}')
+
+area = print ('Por favor informe a area do seu terreno')
+
+largura = float(input('digite a largura : '))
+comprimento = float(input('digite o comprimento: '))
+somar(largura,comprimento) 
+
+
+
+
+
