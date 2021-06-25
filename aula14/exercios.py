2c4d68561d813a9526e683ebec6fb9f564fb16b7
Raimundo <raimundo_l@hotmail.com>
2021-06-25 19:47:52 -0300

    alterações

---
+++ aula14/exercios.py
@@ -0,0 +1,34 @@
+# def calhoras (salario,horasExtras):
+#     horasTotal = 40 + horasExtras
+#     if horasTotal >40:
+#         sala
+        
+
+
+
+
+
+# def somaImposto(taxaImposto, custo):
+#     somaImposto = (taxaImposto/100*custo)
+#     soma = somaImposto + custo
+
+#     taxaImposto = int(input('Digite o valor em porcentagem a ser aumentado \n'))
+#     custo = float(input("Digte o valor do produto"))
+#     var = taxaImposto + custo
+#     somaImposto(taxaImposto,150)
+
+meses = {
+    '01': 'Janeiro',
+    '02': 'Fevereiro ',
+    '03': 'Março',
+    '04': 'Abril',
+    '05': 'Março',
+    '06': 'Maio',
+    '07': 'Junho',
+    '08': 'Julho',
+    '08': 'Agosto',
+    '09': 'Setembro',
+    '10': 'Outubro',
+    '11': 'Novembro',
+    '12': 'Dezembro',
+}
