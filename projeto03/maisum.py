d230bc8974901a85d82172cd2a3af88fc633eaa5
Raimundo <raimundo_l@hotmail.com>
2021-06-30 19:40:35 -0300

    novos exemplos

--- projeto03/maisum.py
+++ projeto03/maisum.py
@@ -17,30 +17,31 @@ def autoriza_voto(ano):
 # else:
 #     print('voce não pode continuar')
 
-candidatos = {
-            '01': 'Capitão Presença',
-            '02': 'Super Aba ',
-            '03': 'Malhado',
-            '04': 'Voto Nulo',
-            '05': 'Voto Branco'
-        }
-def votacao (votos):
+# candidatos = {
+#             '01': 'Capitão Presença',
+#             '02': 'Super Aba ',
+#             '03': 'Malhado',
+#             '04': 'Voto Nulo',
+#             '05': 'Voto Branco'
+#         }
+# def votacao (votos):
 
 
     
-    votos = {} # dicionário com total de votos começa vazio
-    voto = input('Digite o número do candidato (ou "fim" para encerrar): ')
-    if voto in candidatos: # se é um dos números de candidato válido
-            votos[voto] = votos.get(voto, 0) + 1
-    else:
-            print(f'Número inválido: {voto}')
+#     votos = {} # dicionário com total de votos começa vazio
+#     voto = input('Digite o número do candidato (ou "fim" para encerrar): ')
+#     if voto in candidatos: # se é um dos números de candidato válido
+#             votos[voto] = votos.get(voto, 0) + 1
+#     else:
+#             print(f'Número inválido: {voto}')
 
-    #print('Resultado:')
-    for numero, qtd_votos in votos.items():
-        print(f'{candidatos[numero]} teve {qtd_votos} votos')
+#     #print('Resultado:')
+#     for numero, qtd_votos in votos.items():
+        
 
 
 # programa principal
 nas = int(input('em que ano voce nasceu? \n'))
-print (f'escolha seu canditado {candidatos}')
-print(votacao)
+# print (f'escolha seu canditado {candidatos}')
+# #print(votacao)
+#     print(f'{candidatos[numero]} teve {qtd_votos} votos')
